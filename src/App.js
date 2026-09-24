8db0b3f63b226ea185befedf74cdfebb4bfdab47
Daniel <kramardaniel12@gmail.com>
2026-09-24 15:32:27 +0500

    new update

--- src/App.js
+++ src/App.js
@@ -58,6 +58,45 @@ const App = () => {
     document.getElementById("info-text").textContent = JSON.stringify(data, null, 2);
   } 
 
+  const updateUserData = async () => {
+    await fetch("http://localhost:8080/update", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        id: id,
+        name: name,
+        age: age,
+        position: position,
+        salary: salary
+      })
+    });
+
+    setId("")
+    setName("")
+    setAge("")
+    setPosition("")
+    setSalary("")
+  }
+
+  const deleteUser = async () => {
+    const response = await fetch(`http://localhost:8080/delete/${id}`, {
+      method: "DELETE"
+    });
+
+    if(response.ok) {
+      showDeleteResult();
+    }
+
+    setId("");
+  }
+
+  function showDeleteResult() {
+    const result = document.getElementById("result-delete");
+    result.textContent = "user was deleted !";
+  }
+
   return(
     <div className="container">
       <div className="header">
@@ -132,7 +171,77 @@ const App = () => {
       <PageLine />
 
       <div className="block-update">
+        <div className="title"><p>3. Update</p></div>
 
+        
+        <div className="form">
+          <NumberPole 
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+          placeholder={"id.."}
+          className="form-element"
+          />
+
+          <TextPole
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder={"name.."}
+          className="form-element"
+          />
+
+          <NumberPole 
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+          placeholder={"age.."}
+          className="form-element"
+          />
+
+          <TextPole 
+          value={position}
+          onChange={(e) => setPosition(e.target.value)}
+          placeholder={"position.."}
+          className="form-element"
+          />
+
+          <NumberPole 
+          value={salary}
+          onChange={(e)=>setSalary(e.target.value)}
+          placeholder={"salary.."}
+          className="form-element"
+          />
+
+          <SimpleButton 
+          text="okay" 
+          
+          onClick={updateUserData}
+          />
+
+        </div>
+      </div>
+
+      <PageLine />
+
+      <div className="block-delete">
+        <div className="title"><p>4. Delete</p></div>
+
+        <div className="id-delete">
+          <p>Write name of user which you want delete.</p>
+
+          <NumberPole
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+          placeholder="id.."
+          className={"form-element"}
+          id="id-to-delete"
+          />
+
+          <SimpleButton 
+          text="okay"
+          onClick={deleteUser}
+          />
+
+          <h1 id="result-delete"></h1>
+        </div>
       </div>
     </div>
   )
